39ce5e045ed2241dfc48757c8bf8e1e2ab6d8bbe
tsujintsujin <justin.masiga.94@gmail.com>
2022-11-20 17:11:04 +0800

    Alpha_Story_responsive

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,38 +1,43 @@
 
 
 
-function gift_windowSwitch(id){
-    document.getElementById("kitchen_row").style.display = "none";
-    document.getElementById("dining_row").style.display = "none";
-    document.getElementById("living_row").style.display = "none";
-    document.getElementById("bedroom_row").style.display = "none";
-    document.getElementById(id+"_row").style.display = "flex";
+function gift_windowSwitch(id) {
+  document.getElementById("kitchen_row").style.display = "none";
+  document.getElementById("dining_row").style.display = "none";
+  document.getElementById("living_row").style.display = "none";
+  document.getElementById("bedroom_row").style.display = "none";
+  document.getElementById(id + "_row").style.display = "flex";
 }
 
-function add_pledge(id){
-const checkbox = document.getElementById(id)
-checkbox.addEventListener('change', (event) => {
-  if (event.currentTarget.checked) {
-    to_pledge(id);
+function add_pledge(id) {
+  const checkbox = document.getElementById(id)
+  checkbox.addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+      to_pledge(id);
+    } else {
+
+    }
+  })
+}
+
+
+
+
+function to_pledge(id) {
+  var pledgetemp = document.getElementById("pledge_text").innerHTML;
+  document.getElementById("pledge_text").innerHTML = pledgetemp + " " + id;
+}
+
+
+function playPause() {
+  var vid = document.getElementById("Sitevid");
+  if (vid.paused) {
+    vid.play();
+    document.getElementById("playbtn").style.display = "none";
   } else {
+    vid.pause();
+    document.getElementById("playbtn").style.display = "block";
 
   }
-})
 }
 
-
-
-
-function to_pledge(id){
-var pledgetemp = document.getElementById("pledge_text").innerHTML;
-  document.getElementById("pledge_text").innerHTML = pledgetemp +" "+ id;
-}
-
-
-function pledge_check(){
-
-}
-
-function remove_pledge(){
-  
-}
