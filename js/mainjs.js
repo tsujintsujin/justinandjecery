02f23a3cbb10d446275adccfd6faa859979e54a9
tsujintsujin <justin.masiga.94@gmail.com>
2022-11-24 05:40:57 +0800

    weddii final output

--- js/mainjs.js
+++ js/mainjs.js
@@ -50,3 +50,18 @@ function to_pledge(id) {
   document.getElementById("pledge_text").innerHTML = pledgetemp + " " + id;
 }
 
+var countDownDate = new Date("Dec 15, 2022 10:00:00").getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("weddingDay").innerHTML = days + "d " + hours + "h " + "to go!";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("weddingDay").innerHTML = "ITS OUR WEDDING DAY!";
+  }
+}, 1000);
